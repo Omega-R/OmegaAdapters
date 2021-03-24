107d034125ce0a59cd3dc11388222f405effec1d
Anton Knyazev <anton.knyazev@omega-r.com>
2021-03-24 19:33:48 +0300

    add delegates for list adapter

--- library/src/main/java/com/omega_r/adapters/OmegaListableAdapter.kt
+++ library/src/main/java/com/omega_r/adapters/OmegaListableAdapter.kt
@@ -1,10 +1,18 @@
 package com.omega_r.adapters
 
+import kotlin.reflect.KProperty
+
 /**
  * Created by Anton Knyazev on 2019-07-15.
  */
 interface OmegaListableAdapter<T> {
 
     var list: List<T>
+
+    private operator fun setValue(any: Any?, property: KProperty<*>, list: List<T>) {
+        this.list = list
+    }
+
+    private operator fun getValue(any: Any?, property: KProperty<*>): List<T> = list
 
 }
